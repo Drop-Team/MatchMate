ac3a81572c2411e2c55bbff0109672c7443ce9e3
AtomicScience <d.greblyuk@innopolis.university>
2023-02-05 18:20:41 +0300

    Messages done

--- messages.py
+++ messages.py
@@ -1,12 +1,53 @@
-WelcomeMessage = lambda full_name: f'Hello, <b>{message.from_user.full_name}!</b>'
-AliasError = 'Wrong alias format or you tried to match with yourself'
+WelcomeMessage = '''Hello, <b>{full_name}!</b>
+I am MatchMate 🤖 - a bot that helps people to find each other!
 
-Throttle = 'Throttle error'
-FirstlyRemove = 'Firstly remove'
+Privately submit a Telegram handle of a person, and when they decide to submit yours - you both will be notified about the match ❤️‍🔥
 
-Match = 'Match'
-MatchMessageToAnotherPerson = 'Match from another person'
-MatchPending = 'Match Pending'
+Now, type and send the handle of a person with @ symbol
+⏰ Be accurate, because you will be able to change it only in 24 hours!
 
-NoMatchesDuringRemove = 'No Matches Found'
-MatchRemoved = 'MatchRemoved'
+❗️Your matches are stored in a hashed and undecryptable form
+'''
+AliasError = '''‼️ Wrong alias!
+You have either tried to add yourself, or forgot the @ symbol!
+
+Now try again.
+'''
+
+Throttle = '''⏰ You have submitted another alias too soon!
+
+You should wait for at least 24 hours before submitting another alias!
+'''
+FirstlyRemove = '''✏️ You cannot have two matches submitted on the same time!
+
+No, I cannot simply remove your previous alias by myself - because it's encrypted!
+
+Please, use the /unmatch command and follow its instructions to remove your current match!
+'''
+
+Match = '''OMG, it's a match!!! ❤️‍🔥❤️‍🔥❤️‍🔥
+@{alias} has picked you as their match as well!
+'''
+MatchMessageToAnotherPerson = '''OMG, it's a match!!! ❤️‍🔥❤️‍🔥❤️‍🔥
+@{alias} has picked you as their match as well!
+'''
+MatchPending = '''Your match has been saved successfully
+
+Now we should wait. If they decide to submit your handle - both of you will be messaged'''
+
+NoMatchesDuringRemove = '''You don't seem to be having any matches right now...
+Try to submit a new one!
+'''
+MatchRemoved = ''' 💾 Match successfully removed.
+
+Now you can submit another person's handle'''
+
+NoAlias = '''⁉️ You do not seem to have an alias!
+
+I won't be able to match you with anyone!
+
+Add an alias to your Telegram account and come back, I will wait!
+'''
+
+UnmatchUsage = '''📖 Type "/unmatch @(Alias of your current match)" to delete your current match!
+'''
